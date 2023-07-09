eed14812c6baf19787461395548f5c1965f0196a
AllanC12 <allandevfront@gmail.com>
2023-07-09 13:20:09 -0300

    iniciando projeto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
+
+function App() {
+
+  return (
+    <>
+     <Home/>
+     <About/>
+    </>
+  )
+}
+
+export default App
