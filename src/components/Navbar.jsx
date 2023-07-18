cdf6f3669e46efafec219a88b5e91f5f955d9bcc
AllanC12 <allandevfront@gmail.com>
2023-07-17 21:36:45 -0300

    dados coletados do input de busca

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,32 @@ import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 
 import { useState } from "react";
 import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const [search, setSearch] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearch('')
+    console.log(search);
+  };
+
   return (
     <nav id="navbar">
       <h2>
-        <Link to="/"><BiCameraMovie/>BestMovies</Link>
+        <Link to="/">
+          <BiCameraMovie />
+          BestMovies
+        </Link>
       </h2>
-      <form>
-        <input type="text" placeholder="Busque por um filme" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Busque por um filme"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search || ''}
+        />
         <button type="submit">
           <BiSearchAlt2 />
         </button>
